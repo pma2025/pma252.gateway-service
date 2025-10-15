5f63c64192cc67314bc4311177c5eebe1b99a963
luabatepietro <luabatepietroapple@icloud.com>
2025-10-15 12:43:06 -0300

    gateway

--- src/main/java/store/gateway/GatewayResource.java
+++ src/main/java/store/gateway/GatewayResource.java
@@ -6,24 +6,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class GatewayResource {
 
     @GetMapping("/health-check")
     public ResponseEntity<Map<String, String>> healthCheck() {
         return ResponseEntity.ok()
-            .body(Map.of(
-                "osArch", System.getProperty("os.arch"),
-                "osName", System.getProperty("os.name"),
-                "osVersision", System.getProperty("os.version")
-            ));
+                .body(Map.of(
+                        "osArch", System.getProperty("os.arch"),
+                        "osName", System.getProperty("os.name"),
+                        "osVersision", System.getProperty("os.version")));
     }
 
     @GetMapping("/")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok()
-            .body("API for Store");
+                .body("API for Store by In5p3R");
     }
-    
+
 }
